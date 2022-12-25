99dfcf7de2a1a569e406456697aff729835303f7
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-12-25 22:04:12 +0200

    add home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 
 const Header = lazy(() => import('./Header/Header'));
-// const Home = lazy(() => import('pages/Home'));
+const Home = lazy(() => import('pages/Home'));
 // const Movies = lazy(() => import('pages/Movies'));
 // const MovieDetails = lazy(() => import('pages/MovieDetails'));
 
@@ -13,8 +13,8 @@ export const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Header />}>
-          {/* <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
+          <Route index element={<Home />} />
+          {/* <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />

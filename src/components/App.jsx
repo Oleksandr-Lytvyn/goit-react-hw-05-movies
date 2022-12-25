8f74d95daa0e1bcd53a91fd0c085c7ceced1c256
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-12-25 21:50:25 +0200

    copy and mute app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+// import { Cast } from './Cast/Cast';
+// import { Reviews } from './Reviews/Reviews';
+// import { lazy } from 'react';
+
+// const Header = lazy(() => import('./Header/Header'));
+// const Home = lazy(() => import('pages/Home'));
+// const Movies = lazy(() => import('pages/Movies'));
+// const MovieDetails = lazy(() => import('pages/MovieDetails'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {/* <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<Header />} />
+      </Routes> */}
+    </>
   );
 };

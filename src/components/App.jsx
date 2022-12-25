e77a69f8fcbb8cbaf91898e4aa6ea4e7c963786e
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-12-25 22:01:19 +0200

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // import { Cast } from './Cast/Cast';
 // import { Reviews } from './Reviews/Reviews';
-// import { lazy } from 'react';
+import { lazy } from 'react';
 
-// const Header = lazy(() => import('./Header/Header'));
+const Header = lazy(() => import('./Header/Header'));
 // const Home = lazy(() => import('pages/Home'));
 // const Movies = lazy(() => import('pages/Movies'));
 // const MovieDetails = lazy(() => import('pages/MovieDetails'));
@@ -11,18 +11,18 @@
 export const App = () => {
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
+          {/* <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
-          </Route>
+          </Route> */}
         </Route>
 
         <Route path="*" element={<Header />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 };

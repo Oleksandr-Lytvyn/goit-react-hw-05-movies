5cf832a5c602de5d3d75ef05a86e31eaded99064
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-12-25 22:15:03 +0200

    add movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { lazy } from 'react';
 
 const Header = lazy(() => import('./Header/Header'));
 const Home = lazy(() => import('pages/Home'));
-// const Movies = lazy(() => import('pages/Movies'));
+const Movies = lazy(() => import('pages/Movies'));
 // const MovieDetails = lazy(() => import('pages/MovieDetails'));
 
 export const App = () => {
@@ -14,8 +14,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<Home />} />
-          {/* <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="movies" element={<Movies />} />
+          {/* <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route> */}
